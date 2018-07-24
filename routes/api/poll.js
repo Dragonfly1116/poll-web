816c3945136a9f93f7e45d52d6bfbe67aa17a384
Dragonfly <tatsuya10199@gmail.com>
2018-07-25 00:11:32 +0700

    change polls api

--- routes/api/poll.js
+++ routes/api/poll.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const Poll = require('../../models/Poll')
 
-router.get('/', (req,res) => {
+router.get('/polls', (req,res) => {
     Poll.find()
         .sort({date: -1 })
         .then(polls => res.json(polls))
@@ -25,6 +25,26 @@ router.delete('/polls', (req,res) => {
             console.log(err)
         } else {
             res.json('success');
+        }
+    })
+});
+
+router.get('/polls/:id', (req,res) => {
+    Poll.findById({_id:req.params.id})
+        .then(poll => res.json(poll))
+});
+
+router.put('/polls/:id', (req,res) => {
+    Poll.findOneAndUpdate({_id:req.params.id},req.body)
+        .then(poll => res.json(poll))
+});
+
+router.delete('/polls/:id', (req,res) => {
+    Poll.findByIdAndRemove({_id:req.params.id}, (err,p) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(`remove success => ${p}`);
         }
     })
 });
